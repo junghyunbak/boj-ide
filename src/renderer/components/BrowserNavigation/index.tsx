e66976d316e14e8fd99e0b7ecc16dc3e4ede6960
jeong57281 <jeong5728@gmail.com>
2024-11-03 15:53:36 +0900

    style: bojView 네비게이션 스타일링

--- src/renderer/components/BrowserNavigation/index.tsx
+++ src/renderer/components/BrowserNavigation/index.tsx
@@ -16,20 +16,63 @@ export function BrowserNavigation() {
     <div
       className={css`
         width: 100%;
-        height: 40px;
         box-sizing: border-box;
-        border-bottom: 1px solid black;
+        border-bottom: 1px solid lightgray;
         background-color: white;
         display: flex;
         align-items: center;
+        padding: 0.3rem 0.5rem;
+        gap: 0.2rem;
+
+        button {
+          border: none;
+          width: 35px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 0;
+          cursor: pointer;
+          border-radius: 9999px;
+          transition: all ease 0.2s;
+          background-color: transparent;
+          padding: 0.5rem;
+
+          &:hover {
+            background-color: #ececec;
+          }
+
+          svg {
+            width: 100%;
+            color: gray;
+          }
+        }
       `}
     >
-      <button type="button" onClick={handleGoBackButtonClick}>
-        {'<-'}
+      <button type="button" onClick={handleGoBackButtonClick} aria-label="go-back-button">
+        <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+          <path fill="currentColor" d="M224 480h640a32 32 0 110 64H224a32 32 0 010-64z" />
+          <path
+            fill="currentColor"
+            d="M237.248 512l265.408 265.344a32 32 0 01-45.312 45.312l-288-288a32 32 0 010-45.312l288-288a32 32 0 1145.312 45.312L237.248 512z"
+          />
+        </svg>
       </button>
 
-      <button type="button" onClick={handleGoFrontButtonClick}>
-        {'->'}
+      <button
+        type="button"
+        onClick={handleGoFrontButtonClick}
+        aria-label="go-front-button"
+        className={css`
+          transform: rotate(180deg);
+        `}
+      >
+        <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+          <path fill="currentColor" d="M224 480h640a32 32 0 110 64H224a32 32 0 010-64z" />
+          <path
+            fill="currentColor"
+            d="M237.248 512l265.408 265.344a32 32 0 01-45.312 45.312l-288-288a32 32 0 010-45.312l288-288a32 32 0 1145.312 45.312L237.248 512z"
+          />
+        </svg>
       </button>
     </div>
   );
