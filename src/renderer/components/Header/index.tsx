1468995d3ae5dae788ec67024c650627634e1f61
jeong57281 <jeong5728@gmail.com>
2024-11-03 15:36:54 +0900

    style: header 컴포넌트 스타일링

--- src/renderer/components/Header/index.tsx
+++ src/renderer/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { css } from '@emotion/css';
+
 import { useEffect } from 'react';
 
 import { useShallow } from 'zustand/shallow';
@@ -11,7 +13,7 @@ export function Header() {
 
   const [ext] = useStore(useShallow((s) => [s.ext]));
 
-  const [setMode] = useStore(useShallow((s) => [s.setMode]));
+  const [mode, setMode] = useStore(useShallow((s) => [s.mode, s.setMode]));
 
   const [isJudging, setIsJudging] = useStore(useShallow((s) => [s.isJudging, s.setIsJudging]));
 
@@ -48,35 +50,120 @@ export function Header() {
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-      <p>{problem === null ? '문제 페이지로 이동하세요.' : problem.number}</p>
+    <div
+      className={css`
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        padding: 1rem;
+        border-bottom: 1px solid lightgray;
+      `}
+    >
+      <p
+        className={css`
+          margin: 0;
+        `}
+      >
+        {problem === null ? '문제 페이지로 이동하세요.' : `${problem.number}.${ext}`}
+      </p>
 
-      <div>
-        <button
-          type="button"
-          onClick={() => {
-            setMode('normal');
-          }}
+      <div
+        className={css`
+          display: flex;
+        `}
+      >
+        <div
+          className={css`
+            display: flex;
+            border: 1px solid lightgray;
+            border-radius: 4px;
+            overflow: hidden;
+            margin-right: 0.5rem;
+
+            button {
+              transition: all ease 0.2s;
+              padding: 0.4rem 0.8rem;
+              cursor: pointer;
+
+              &:hover {
+                background-color: lightgray;
+                color: white;
+              }
+
+              p {
+                margin: 0;
+              }
+            }
+          `}
         >
-          normal
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            setMode('vim');
-          }}
+          <button
+            className={css`
+              border: none;
+              background-color: ${mode === 'normal' ? 'lightgray' : 'transparent'};
+              color: ${mode === 'normal' ? 'white' : 'black'};
+            `}
+            type="button"
+            onClick={() => {
+              setMode('normal');
+            }}
+          >
+            normal
+          </button>
+
+          <button
+            className={css`
+              border: none;
+              background-color: ${mode === 'vim' ? 'lightgray' : 'transparent'};
+              color: ${mode === 'vim' ? 'white' : 'black'};
+            `}
+            type="button"
+            onClick={() => {
+              setMode('vim');
+            }}
+          >
+            vim
+          </button>
+        </div>
+
+        <div
+          className={css`
+            display: flex;
+            gap: 0.5rem;
+
+            button {
+              border: none;
+              border-radius: 4px;
+              color: white;
+              padding: 0.4rem 0.8rem;
+              cursor: pointer;
+              background-color: #428bca;
+
+              &:disabled {
+                background-color: gray;
+                cursor: auto;
+                color: lightgray;
+
+                &:hover {
+                  background-color: gray;
+                }
+              }
+
+              &:hover {
+                background-color: #2980b9;
+              }
+            }
+          `}
         >
-          vim
-        </button>
+          <button type="button" onClick={handleSaveButtonClick}>
+            저장하기
+          </button>
+
+          <button type="button" onClick={handleSubmitButtonClick} disabled={isJudging}>
+            제출하기
+          </button>
+        </div>
       </div>
-
-      <button type="button" onClick={handleSaveButtonClick}>
-        저장하기
-      </button>
-
-      <button type="button" onClick={handleSubmitButtonClick} disabled={isJudging}>
-        제출하기
-      </button>
     </div>
   );
 }
