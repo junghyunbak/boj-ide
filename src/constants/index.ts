5a2a2b1247446bfd9c5be5bbebdb52e5e5634302
jeong57281 <jeong5728@gmail.com>
2024-11-14 14:40:14 +0900

    chore: 상수 파일 분리

---
+++ src/constants/index.ts
@@ -0,0 +1,39 @@
+export const JS_INPUT_TEMPLATE = `const fs = require('fs');
+
+const stdin = fs.readFileSync(process.platform === 'linux' ? 0 : 'input', 'utf-8').split('\\n');
+
+const input = (() => {
+  let i = -1;
+
+  return () => stdin[++i];
+})();
+
+/*
+[javascript input template]
+
+input 함수를 사용하면 표준 입력을 한줄씩 읽어올 수 있습니다.
+
+예제 - A+B (https://www.acmicpc.net/problem/1000)
+*/
+
+const [a, b] = input().split(' ').map(Number);
+
+console.log(a + b);
+`;
+
+export const CPP_INPUT_TEMPLATE = `#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+
+  int a, b;
+
+  cin >> a >> b;
+
+  cout << a+b << endl;
+
+  return 0;
+}
+`;
