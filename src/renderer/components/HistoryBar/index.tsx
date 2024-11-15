1fc12b6c654fd8f30b3f56ab7f7c0ed2151e0ecc
jeong57281 <jeong5728@gmail.com>
2024-11-15 10:57:34 +0900

    style: 히스토리바 활성화 시 border로 인한 스타일 오차 수정

--- src/renderer/components/HistoryBar/index.tsx
+++ src/renderer/components/HistoryBar/index.tsx
@@ -89,70 +89,78 @@ export function HistoryBar() {
             type="button"
             key={number}
             className={css`
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              padding: 0 0.7rem;
-              gap: 0.5rem;
               background-color: transparent;
               border-top: 0;
-              border-left: 0;
-              border-bottom: 2px solid ${problem?.number === number ? '#428bca' : 'transparent'};
+              border: 0;
               border-right: 1px solid lightgray;
+              padding: 0;
             `}
             onClick={() => {
               setProblem(problemInfo);
               window.electron.ipcRenderer.sendMessage('go-problem', { data: problemInfo });
             }}
           >
-            <p
+            <div
               className={css`
-                margin: 0;
-                white-space: nowrap;
-              `}
-            >
-              {`${number}번: ${name}`}
-            </p>
-            <button
-              type="button"
-              className={css`
-                border: none;
-                border-radius: 9999px;
-                width: 15px;
-                height: 15px;
-                background-color: transparent;
-                padding: 1px;
                 display: flex;
                 justify-content: center;
                 align-items: center;
-
-                &:hover {
-                  background-color: lightgray;
-                  cursor: pointer;
-                }
+                padding: 0 0.7rem;
+                gap: 0.5rem;
+                width: 100%;
+                height: 100%;
+                border-bottom: 2px solid ${problem?.number === number ? '#428bca' : 'transparent'};
               `}
-              aria-label="tab-close-button"
-              onClick={() => {
-                removeProblemHistory(i);
-              }}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
+              <p
                 className={css`
-                  width: 100%;
-                  height: 100%;
+                  margin: 0;
+                  white-space: nowrap;
                 `}
               >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z"
-                  fill="#0F1729"
-                />
-              </svg>
-            </button>
+                {`${number}번: ${name}`}
+              </p>
+              <button
+                type="button"
+                className={css`
+                  border: none;
+                  border-radius: 9999px;
+                  width: 15px;
+                  height: 15px;
+                  background-color: transparent;
+                  padding: 1px;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+
+                  &:hover {
+                    background-color: lightgray;
+                    cursor: pointer;
+                  }
+                `}
+                aria-label="tab-close-button"
+                onClick={() => {
+                  removeProblemHistory(i);
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  className={css`
+                    width: 100%;
+                    height: 100%;
+                  `}
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z"
+                    fill="#0F1729"
+                  />
+                </svg>
+              </button>
+            </div>
           </button>
         );
       })}
