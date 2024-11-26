eb73d8e353491b4d1b49abfdb076b61622f698b7
junghyunbak <jeong5728@gmail.com>
2024-11-26 15:18:00 +0900

    refactor: 히스토리바  리팩토링

---
+++ src/renderer/components/HistoryBar/index.styles.ts
@@ -0,0 +1,66 @@
+import styled from '@emotion/styled';
+import { size, color } from '@/styles';
+
+export const HistoryBarLayout = styled.div`
+  border-bottom: 1px solid lightgray;
+  display: flex;
+  overflow-x: scroll;
+  min-height: ${size.HISTORY_BAR_HEIGHT}px;
+  height: ${size.HISTORY_BAR_HEIGHT}px;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const HistoryBarItemLayout = styled.div`
+  border-right: 1px solid lightgray;
+  padding: 0;
+`;
+
+export const HistoryBarItemContentBox = styled.div<{ isSelect: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+
+  width: 100%;
+  height: 100%;
+
+  padding: 0 0.7rem;
+
+  border-bottom: 2px solid ${(props) => (props.isSelect ? '#428bca' : 'transparent')};
+`;
+
+export const HistoryBarItemContentParagraph = styled.p`
+  margin: 0;
+  white-space: nowrap;
+  font-size: 0.875rem;
+  color: ${color.text};
+`;
+
+export const HistoryBarItemCloseButton = styled.button`
+  border: none;
+  border-radius: 9999px;
+
+  width: 15px;
+  height: 15px;
+  padding: 1px;
+
+  background-color: transparent;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    background-color: lightgray;
+    cursor: pointer;
+  }
+
+  svg {
+    width: 100%;
+    height: 100%;
+    color: ${color.text};
+  }
+`;
--- src/renderer/components/HistoryBar/index.tsx
+++ src/renderer/components/HistoryBar/index.tsx
@@ -1,8 +1,18 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { MouseEventHandler } from 'react';
 import { useShallow } from 'zustand/shallow';
-import { css } from '@emotion/react';
 import { useStore } from '@/renderer/store';
-import { color, size } from '@/styles';
 import { useXScroll } from '@/renderer/hooks';
+import { ReactComponent as X } from '@/renderer/assets/svgs/x.svg';
+
+import {
+  HistoryBarItemLayout,
+  HistoryBarItemCloseButton,
+  HistoryBarItemContentBox,
+  HistoryBarItemContentParagraph,
+  HistoryBarLayout,
+} from './index.styles';
 
 export function HistoryBar() {
   const [problem, setProblem] = useStore(useShallow((s) => [s.problem, s.setProblem]));
@@ -13,113 +23,46 @@ export function HistoryBar() {
 
   const { xScrollRef } = useXScroll();
 
+  const handleHistoryBarItemClick = (problemInfo: ProblemInfo) => () => {
+    setProblem(problemInfo);
+    window.electron.ipcRenderer.sendMessage('go-problem', { data: problemInfo });
+  };
+
+  const handleHistoryBarItemCloseButtonClick =
+    (problemInfo: ProblemInfo, index: number): MouseEventHandler<HTMLButtonElement> =>
+    (e) => {
+      removeProblemHistory(index);
+
+      if (problem?.number === problemInfo.number) {
+        setProblem(null);
+
+        window.electron.ipcRenderer.sendMessage('go-problem', { data: null });
+      }
+
+      e.stopPropagation();
+    };
+
   return (
-    <div
-      css={css`
-        border-bottom: 1px solid lightgray;
-        display: flex;
-        overflow-x: scroll;
-        min-height: ${size.HISTORY_BAR_HEIGHT}px;
-        height: ${size.HISTORY_BAR_HEIGHT}px;
-
-        &::-webkit-scrollbar {
-          display: none;
-        }
-      `}
-      ref={xScrollRef}
-    >
-      {problemHistories.map((problemInfo, i) => {
-        const { number, name } = problemInfo;
-
+    <HistoryBarLayout ref={xScrollRef}>
+      {problemHistories.map((problemInfo, index) => {
         return (
-          <button
-            type="button"
-            key={number}
-            css={css`
-              background-color: transparent;
-              border-top: 0;
-              border: 0;
-              border-right: 1px solid lightgray;
-              padding: 0;
-            `}
-            onClick={() => {
-              setProblem(problemInfo);
-              window.electron.ipcRenderer.sendMessage('go-problem', { data: problemInfo });
-            }}
-          >
-            <div
-              css={css`
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                padding: 0 0.7rem;
-                gap: 0.5rem;
-                width: 100%;
-                height: 100%;
-                border-bottom: 2px solid ${problem?.number === number ? '#428bca' : 'transparent'};
-                color: ${color.text};
-              `}
-            >
-              <p
-                css={css`
-                  margin: 0;
-                  white-space: nowrap;
-                `}
-              >
-                {`${number}번: ${name}`}
-              </p>
-              <button
+          <HistoryBarItemLayout key={problemInfo.number} onClick={handleHistoryBarItemClick(problemInfo)}>
+            <HistoryBarItemContentBox isSelect={problem?.number === problemInfo.number}>
+              <HistoryBarItemContentParagraph>
+                {`${problemInfo.number}번: ${problemInfo.name}`}
+              </HistoryBarItemContentParagraph>
+
+              <HistoryBarItemCloseButton
                 type="button"
-                css={css`
-                  border: none;
-                  border-radius: 9999px;
-                  width: 15px;
-                  height: 15px;
-                  background-color: transparent;
-                  padding: 1px;
-                  display: flex;
-                  justify-content: center;
-                  align-items: center;
-
-                  &:hover {
-                    background-color: lightgray;
-                    cursor: pointer;
-                  }
-                `}
                 aria-label="tab-close-button"
-                onClick={(e) => {
-                  removeProblemHistory(i);
-
-                  if (problem?.number === number) {
-                    setProblem(null);
-
-                    window.electron.ipcRenderer.sendMessage('go-problem', { data: null });
-                  }
-
-                  e.stopPropagation();
-                }}
+                onClick={handleHistoryBarItemCloseButtonClick(problemInfo, index)}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  css={css`
-                    width: 100%;
-                    height: 100%;
-                  `}
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z"
-                    fill="#0F1729"
-                  />
-                </svg>
-              </button>
-            </div>
-          </button>
+                <X />
+              </HistoryBarItemCloseButton>
+            </HistoryBarItemContentBox>
+          </HistoryBarItemLayout>
         );
       })}
-    </div>
+    </HistoryBarLayout>
   );
 }
