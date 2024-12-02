d7e33cc8da03c7c886358f2573fedececf1c7e44
junghyunbak <jeong5728@gmail.com>
2024-12-02 09:53:22 +0900

    refactor: bojView 네비게이션 컴포넌트 리팩토링

---
+++ src/renderer/components/BrowserNavigation/index.styles.ts
@@ -0,0 +1,73 @@
+import styled from '@emotion/styled';
+import { size } from '@/styles';
+
+export const BrowserNavigationLayout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  overflow: hidden;
+
+  width: 100%;
+  height: ${size.BOJ_VIEW_NAVIGATION_HEIGHT}px;
+
+  padding: 0.3rem 0.5rem;
+
+  box-sizing: border-box;
+  border-bottom: 1px solid lightgray;
+
+  background-color: white;
+`;
+
+export const BrowserNavigationHistoryBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.2rem;
+`;
+
+export const BrowserNavigationHistoryButton = styled.button<{ horizontalFlip?: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 35px;
+
+  padding: 0.5rem;
+
+  border: none;
+  border-radius: 9999px;
+
+  transition: all ease 0.2s;
+
+  background-color: transparent;
+
+  transform: rotate(${(props) => (props.horizontalFlip ? 180 : 0)}deg);
+
+  &:hover {
+    background-color: #ececec;
+  }
+
+  cursor: pointer;
+
+  svg {
+    width: 100%;
+    color: gray;
+  }
+`;
+
+export const BrowserNavigationBookmarkBox = styled.div`
+  display: flex;
+`;
+
+export const BrowserNavigationBookmarkButton = styled.button`
+  border: none;
+
+  padding: 0.4rem 0.8rem;
+
+  background: none;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  cursor: pointer;
+`;
--- src/renderer/components/BrowserNavigation/index.tsx
+++ src/renderer/components/BrowserNavigation/index.tsx
@@ -1,6 +1,13 @@
 import { css } from '@emotion/react';
-
-import { color, size } from '../../../styles';
+import { ReactComponent as LeftArrow } from '@/renderer/assets/svgs/left-arrow.svg';
+import { color } from '@/styles';
+import {
+  BrowserNavigationBookmarkBox,
+  BrowserNavigationBookmarkButton,
+  BrowserNavigationHistoryBox,
+  BrowserNavigationHistoryButton,
+  BrowserNavigationLayout,
+} from './index.styles';
 
 export function BrowserNavigation() {
   /**
@@ -23,96 +30,24 @@ export function BrowserNavigation() {
   };
 
   return (
-    <div
-      css={css`
-        width: 100%;
-        height: ${size.BOJ_VIEW_NAVIGATION_HEIGHT}px;
-        box-sizing: border-box;
-        border-bottom: 1px solid lightgray;
-        background-color: white;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0.3rem 0.5rem;
-        overflow: hidden;
-      `}
-    >
-      <div
-        css={css`
-          display: flex;
-          align-items: center;
+    <BrowserNavigationLayout>
+      <BrowserNavigationHistoryBox>
+        <BrowserNavigationHistoryButton type="button" onClick={handleGoBackButtonClick} aria-label="go-back-button">
+          <LeftArrow />
+        </BrowserNavigationHistoryButton>
 
-          gap: 0.2rem;
-
-          button {
-            border: none;
-            width: 35px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 0;
-            cursor: pointer;
-            border-radius: 9999px;
-            transition: all ease 0.2s;
-            background-color: transparent;
-            padding: 0.5rem;
-
-            &:hover {
-              background-color: #ececec;
-            }
-
-            svg {
-              width: 100%;
-              color: gray;
-            }
-          }
-        `}
-      >
-        <button type="button" onClick={handleGoBackButtonClick} aria-label="go-back-button">
-          <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-            <path fill="currentColor" d="M224 480h640a32 32 0 110 64H224a32 32 0 010-64z" />
-            <path
-              fill="currentColor"
-              d="M237.248 512l265.408 265.344a32 32 0 01-45.312 45.312l-288-288a32 32 0 010-45.312l288-288a32 32 0 1145.312 45.312L237.248 512z"
-            />
-          </svg>
-        </button>
-
-        <button
+        <BrowserNavigationHistoryButton
           type="button"
           onClick={handleGoFrontButtonClick}
           aria-label="go-front-button"
-          css={css`
-            transform: rotate(180deg);
-          `}
+          horizontalFlip
         >
-          <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-            <path fill="currentColor" d="M224 480h640a32 32 0 110 64H224a32 32 0 010-64z" />
-            <path
-              fill="currentColor"
-              d="M237.248 512l265.408 265.344a32 32 0 01-45.312 45.312l-288-288a32 32 0 010-45.312l288-288a32 32 0 1145.312 45.312L237.248 512z"
-            />
-          </svg>
-        </button>
-      </div>
+          <LeftArrow />
+        </BrowserNavigationHistoryButton>
+      </BrowserNavigationHistoryBox>
 
-      <div
-        css={css`
-          display: flex;
-
-          button {
-            background: none;
-            border: none;
-            cursor: pointer;
-            padding: 0.4rem 0.8rem;
-
-            &:hover {
-              background-color: rgba(0, 0, 0, 0.1);
-            }
-          }
-        `}
-      >
-        <button
+      <BrowserNavigationBookmarkBox>
+        <BrowserNavigationBookmarkButton
           type="button"
           onClick={handleGoSolvedAcButtonClick}
           css={css`
@@ -127,8 +62,9 @@ export function BrowserNavigation() {
           `}
         >
           solved.<span>AC</span>
-        </button>
-        <button
+        </BrowserNavigationBookmarkButton>
+
+        <BrowserNavigationBookmarkButton
           type="button"
           onClick={handleGoBaekjoonButtonClick}
           css={css`
@@ -142,8 +78,8 @@ export function BrowserNavigation() {
           `}
         >
           BAE<span>{'/<'}</span>JOON<span>{'>'}</span>
-        </button>
-      </div>
-    </div>
+        </BrowserNavigationBookmarkButton>
+      </BrowserNavigationBookmarkBox>
+    </BrowserNavigationLayout>
   );
 }
