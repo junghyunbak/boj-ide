4b9ccc42e5914c8c62d8eeb32cdc21373dbe439d
junghyunbak <jeong5728@gmail.com>
2024-12-02 10:04:22 +0900

    refactor: confirm modal 스타일 컴포넌트 분리

---
+++ src/renderer/components/ConfirmModal/index.styles.ts
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+import { color } from '@/styles';
+
+export const ConfirmModalLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: absolute;
+  inset: 0;
+`;
+
+export const ConfirmModalContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+
+  background-color: white;
+
+  padding: 1rem;
+
+  border: 1px solid lightgray;
+`;
+
+export const ConfirmModalMessageParagraph = styled.p`
+  margin: 0;
+  color: ${color.text};
+`;
+
+export const ConfirmModalYesOrNoBox = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
--- src/renderer/components/ConfirmModal/index.tsx
+++ src/renderer/components/ConfirmModal/index.tsx
@@ -1,8 +1,12 @@
 import { useShallow } from 'zustand/shallow';
-import { css } from '@emotion/css';
-import { useStore } from '../../store';
-import { SubmitButton } from '../core/button/SubmitButton';
-import { color } from '../../../styles';
+import { useStore } from '@/renderer/store';
+import { SubmitButton } from '@/renderer/components/core/button/SubmitButton';
+import {
+  ConfirmModalContentBox,
+  ConfirmModalLayout,
+  ConfirmModalMessageParagraph,
+  ConfirmModalYesOrNoBox,
+} from './index.styles';
 
 export function ConfirmModal() {
   const [confirmMessage, callback, setConfirm] = useStore(
@@ -28,45 +32,15 @@ export function ConfirmModal() {
   }
 
   return (
-    <div
-      className={css`
-        position: absolute;
-        inset: 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      `}
-    >
-      <div
-        className={css`
-          background-color: white;
-          padding: 1rem;
-          display: flex;
-          align-items: center;
-          flex-direction: column;
-          border: 1px solid lightgray;
-          gap: 1rem;
-        `}
-      >
-        <p
-          className={css`
-            margin: 0;
-            color: ${color.text};
-          `}
-        >
-          {confirmMessage}
-        </p>
+    <ConfirmModalLayout>
+      <ConfirmModalContentBox>
+        <ConfirmModalMessageParagraph>{confirmMessage}</ConfirmModalMessageParagraph>
 
-        <div
-          className={css`
-            display: flex;
-            gap: 0.5rem;
-          `}
-        >
+        <ConfirmModalYesOrNoBox>
           <SubmitButton onClick={handleOkButtonClick}>예</SubmitButton>
           <SubmitButton onClick={handleNoButtonClick}>아니오</SubmitButton>
-        </div>
-      </div>
-    </div>
+        </ConfirmModalYesOrNoBox>
+      </ConfirmModalContentBox>
+    </ConfirmModalLayout>
   );
 }
