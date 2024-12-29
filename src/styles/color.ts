b41356d578c0b58c5b54a8b47cc5f62762f8241c
junghyunbak <jeong5728@gmail.com>
2024-12-29 13:22:58 +0900

    refactor: 스타일 파일 정리

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,17 +1,9 @@
 export const correct = '#009874';
-
 export const wrong = '#dd4124';
-
 export const error = '#5f4b8b';
-
 export const over = '#fa7268';
-
 export const primaryBg = '#428bca';
-
 export const secondaryBg = '#ba68c8';
-
 export const primaryText = '#0076C0';
-
-export const text = '#333333';
-
 export const judging = '#e67e22';
+export const text = '#333333';
--- src/styles/size.ts
+++
@@ -1,5 +0,0 @@
-export const BOJ_VIEW_NAVIGATION_HEIGHT = 50;
-
-export const EDITOR_HEADER_HEIGHT = 50;
-
-export const HISTORY_BAR_HEIGHT = 35;
