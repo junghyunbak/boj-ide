ff98acac5255fcc67c5046d52fef830102ebdea9
junghyunbak <jeong5728@gmail.com>
2024-12-30 16:06:18 +0900

    chore: sass 제거

--- .erb/configs/webpack.config.renderer.prod.ts
+++ .erb/configs/webpack.config.renderer.prod.ts
@@ -40,25 +40,8 @@ const configuration: webpack.Configuration = {
   module: {
     rules: [
       {
-        test: /\.s?(a|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: true,
-              importLoaders: 1,
-            },
-          },
-          'sass-loader',
-        ],
-        include: /\.module\.s?(c|a)ss$/,
-      },
-      {
-        test: /\.s?(a|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-        exclude: /\.module\.s?(c|a)ss$/,
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
       // Fonts
       {
