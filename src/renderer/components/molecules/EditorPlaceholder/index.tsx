b6d1b5fb62abcffeb990d0bddfa76f1c9b1c2a4a
junghyunbak <jeong5728@gmail.com>
2025-01-02 20:10:25 +0900

    style: editor placeholder logo 제거

--- src/renderer/components/molecules/EditorPlaceholder/index.tsx
+++ src/renderer/components/molecules/EditorPlaceholder/index.tsx
@@ -1,6 +1,5 @@
 import { css } from '@emotion/react';
 import { Text } from '@/renderer/components/atoms/paragraphs/Text';
-import { ReactComponent as Logo } from '@/renderer/assets/svgs/logo.svg';
 
 export function EditorPlaceholder() {
   return (
@@ -15,12 +14,6 @@ export function EditorPlaceholder() {
         gap: 1.5rem;
       `}
     >
-      <Logo
-        css={css`
-          width: 11rem;
-          color: lightgray;
-        `}
-      />
       <Text>왼쪽 브라우저에서 문제 페이지로 이동하세요.</Text>
     </div>
   );
