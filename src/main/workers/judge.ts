e5e52b588b918f0a7ba526c2c727a520c6dacb5f
junghyunbak <jeong5728@gmail.com>
2025-01-05 14:16:38 +0900

    fix: worker 메인 프로세스로 실행되는 이슈 수정

--- src/main/workers/judge.ts
+++ src/main/workers/judge.ts
@@ -1,8 +1,14 @@
 import { parentPort } from 'worker_threads';
 
-import { removeAnsiText, customSpawn } from '@/main/utils';
-
-import { MAX_BUFFER_SIZE } from '@/main/constants';
+/**
+ * @/main/constants, @/main/utils 와 같이 index 파일로 다시 export 하는 폴더구조를 import 할 경우
+ * tree shaking이 실패할 수 있어 사용하길 원치 않는 모듈까지 import 할 수 있다.
+ * worker는 독립적으로 실행되지 않으면 메인프로세스로 동작하여 parentPort가 null이 되어 정상적으로 동작하지 않기 때문에
+ * 다른 모듈(예 - electron)을 import하지 않도록 사용하는 파일만 직접 import 하도록 작성한다.
+ */
+import { MAX_BUFFER_SIZE } from '@/main/constants/process';
+import { customSpawn } from '@/main/utils/spawn';
+import { removeAnsiText } from '@/main/utils/string';
 
 type ExecuteResult = {
   stdout: string;
