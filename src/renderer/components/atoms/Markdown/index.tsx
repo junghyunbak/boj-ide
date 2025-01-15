b17b44bbf1b2743b7ee277a7334aafc031c8a25a
junghyunbak <jeong5728@gmail.com>
2025-01-15 12:29:50 +0900

    feat: markdown gfm, html 문법 적용되도록 수정

--- src/renderer/components/atoms/Markdown/index.tsx
+++ src/renderer/components/atoms/Markdown/index.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/no-unstable-nested-components */
 
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+
 import 'github-markdown-css/github-markdown-light.css';
 
 interface MarkdownProps {
@@ -10,6 +13,8 @@ interface MarkdownProps {
 export function Markdown({ children }: MarkdownProps) {
   return (
     <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      rehypePlugins={[rehypeRaw]}
       className="markdown-body"
       components={{
         a(props) {
