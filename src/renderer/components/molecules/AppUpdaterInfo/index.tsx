5a54c683e396b4bdd1235527d2f9b6c640f1278f
junghyunbak <jeong5728@gmail.com>
2025-01-24 18:21:04 +0900

    feat: bytes에 따라 적당한 단위로 표시하는 유틸 함수 추가

--- src/renderer/components/molecules/AppUpdaterInfo/index.tsx
+++ src/renderer/components/molecules/AppUpdaterInfo/index.tsx
@@ -4,6 +4,15 @@ import { useEffect, useState } from 'react';
 
 import { Text } from '@/renderer/components/atoms/paragraphs/Text';
 
+// https://gist.github.com/lanqy/5193417?permalink_comment_id=4225701#gistcomment-4225701
+function bytesToSize(bytes: number): string {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  if (bytes === 0) return 'n/a';
+  const i = Math.min(Math.floor(Math.log(bytes) / Math.log(1024)), sizes.length - 1);
+  if (i === 0) return `${bytes} ${sizes[i]}`;
+  return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`;
+}
+
 export function AppUpdaterInfo() {
   const [updateInfo, setUpdateInfo] = useState('');
 
@@ -12,9 +21,7 @@ export function AppUpdaterInfo() {
       if (isDownloaded) {
         setUpdateInfo('');
       } else {
-        setUpdateInfo(
-          `업데이트 다운로드 중... ${percent?.toFixed(1)}% (${((bytesPerSecond || 0) / 1024)?.toFixed(1)}KB/s)`,
-        );
+        setUpdateInfo(`업데이트 다운로드 중... ${percent?.toFixed(1)}% (${bytesToSize(bytesPerSecond || 0)}/s)`);
       }
     });
 
