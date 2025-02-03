d82b4ce2b4a2f408e1873ef6b4b2f17be1fbc19e
junghyunbak <jeong5728@gmail.com>
2025-02-04 00:44:28 +0900

    refactor: 불필요한 테스트코드 제거

--- src/renderer/components/molecules/SaveCodeButton/index.tsx
+++ src/renderer/components/molecules/SaveCodeButton/index.tsx
@@ -5,8 +5,6 @@ import { useEditorController } from '@/renderer/hooks';
 
 import { ActionButton } from '@/renderer/components/atoms/buttons/ActionButton';
 
-// [v]: 저장 버튼을 누를 경우 "저장이 완료되었습니다" 메세지가 출력된다.
-// [v]: 저장 버튼을 누를 경우 저장 버튼이 비활성화처리된다.
 export function SaveCodeButton() {
   const [problem] = useStore(useShallow((s) => [s.problem]));
   const [isCodeStale] = useStore(useShallow((s) => [s.isCodeStale]));
