02ec5d5eea6754e47e8d89c2a353d7a7aff694d8
jeong57281 <jeong5728@gmail.com>
2025-02-12 16:23:46 +0900

    refactor: 테스트코드에 사용된 mock 데이터 생성함수로 변경

--- src/renderer/components/molecules/TestCaseMaker/index.test.tsx
+++ src/renderer/components/molecules/TestCaseMaker/index.test.tsx
@@ -3,6 +3,8 @@ import { fireEvent, render, screen } from '@testing-library/react';
 
 import { useStore } from '@/renderer/store';
 
+import { createMockProblem } from '@/renderer/mock';
+
 import { TestCaseMaker } from '.';
 
 describe('문제가 초기화되지 않은 상태', () => {
@@ -16,18 +18,13 @@ describe('문제가 초기화되지 않은 상태', () => {
 });
 
 describe('문제가 초기화 되어있는 상태', () => {
-  it('표준 입력, 출력이 비어있는 상태에서 추가 버튼을 누를 경우 에러 메세지를 설정해야한다.', async () => {
+  beforeAll(() => {
     const { setProblem } = useStore.getState();
 
-    setProblem({
-      name: 'A + B',
-      number: '1000',
-      testCase: {
-        inputs: [],
-        outputs: [],
-      },
-    });
+    setProblem(createMockProblem());
+  });
 
+  it('표준 입력, 출력이 비어있는 상태에서 추가 버튼을 누를 경우 에러 메세지를 설정해야한다.', async () => {
     render(<TestCaseMaker />);
 
     const $addButton = screen.getByRole('button');
