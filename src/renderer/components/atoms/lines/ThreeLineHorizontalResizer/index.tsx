b6e3ff06826785fea618b69a590a9f9f599d0239
junghyunbak <jeong5728@gmail.com>
2025-02-26 23:17:45 +0900

    style: resizer Hover시 스타일 변경 영역 확장

--- src/renderer/components/atoms/lines/ThreeLineHorizontalResizer/index.tsx
+++ src/renderer/components/atoms/lines/ThreeLineHorizontalResizer/index.tsx
@@ -4,35 +4,33 @@ export function ThreeLineHorizontalResizer() {
   return (
     <div
       css={css`
+        width: 100%;
+
         display: flex;
-        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        gap: 0.1rem;
+
+        padding: 0.2rem 0;
+
         cursor: row-resize;
+
+        & > div {
+          width: 40px;
+          height: 2px;
+          background: lightgray;
+        }
+
+        &:hover {
+          & > div {
+            background: gray;
+          }
+        }
       `}
     >
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          padding: 0.2rem 0;
-          gap: 0.1rem;
-
-          & > div {
-            width: 40px;
-            height: 2px;
-            background: lightgray;
-          }
-
-          &:hover {
-            & > div {
-              background: gray;
-            }
-          }
-        `}
-      >
-        <div />
-        <div />
-        <div />
-      </div>
+      <div />
+      <div />
+      <div />
     </div>
   );
 }
