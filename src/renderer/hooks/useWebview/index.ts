de06c7292ef6215d6a89b503cddba08e7524b4be
junghyunbak <jeong5728@gmail.com>
2025-03-01 04:09:34 +0900

    style: 문제 페이지에도 테마 적용

--- src/renderer/hooks/useWebview/index.ts
+++ src/renderer/hooks/useWebview/index.ts
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { useStore } from '@/renderer/store';
 import { useShallow } from 'zustand/shallow';
 
+import { useTheme as useEmotionTheme } from '@emotion/react';
+
 import { getProblemInfo } from '@/renderer/utils';
 
 import { BOJ_DOMAIN } from '@/common/constants';
@@ -10,14 +12,115 @@ import { BOJ_DOMAIN } from '@/common/constants';
 import { useWebviewController } from '../useWebviewController';
 import { useTab } from '../useTab';
 import { useProblem } from '../useProblem';
+import { useTheme } from '../useTheme';
 
 export function useWebview() {
   const [webview, setWebview] = useStore(useShallow((s) => [s.webview, s.setWebview]));
-  const [startWebviewUrl, setStartWebviewUrl] = useState(useStore.getState().webviewUrl);
+  const [webviewUrl] = useStore(useShallow((s) => [s.webviewUrl]));
+  const [startWebviewUrl, setStartWebviewUrl] = useState(webviewUrl);
 
+  const emotionTheme = useEmotionTheme();
+
+  const { theme } = useTheme();
   const { addProblemTab } = useTab();
   const { updateProblem } = useProblem();
   const { updateWebviewUrl } = useWebviewController();
+
+  useEffect(() => {
+    if (!webview) {
+      return;
+    }
+
+    const customStyleId = 'custom-style';
+
+    webview
+      .executeJavaScript(
+        `
+      (() => {
+        const $newStyleDiv = document.createElement('div');
+        $newStyleDiv.id = '${customStyleId}';
+
+        const $customStyleDiv = document.querySelector('#${customStyleId}');
+
+        if($customStyleDiv) {
+          $customStyleDiv.remove();
+        }
+
+        $newStyleDiv.innerHTML = \`
+          <style>
+            .wrapper {
+              background: ${emotionTheme.colors.bg};
+            }
+
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6 {
+              color: ${emotionTheme.colors.primaryfg};
+            }
+
+            .headline h2,
+            .headline h3,
+            .headline h4 { 
+              border-color: ${emotionTheme.colors.primarybg};
+            }
+            
+            body,
+            li,
+            a,
+            p {
+              color: ${emotionTheme.colors.fg} !important;
+            }
+
+            .active a {
+              background-color: ${emotionTheme.colors.primarybg} !important;
+            }
+
+            .header,
+            .page-header,
+            .table-responsive,
+            .table-responsive td,
+            .headline {
+              border-color: ${emotionTheme.colors.border} !important;
+            }
+
+            .sampledata { 
+              background-color: ${emotionTheme.colors.code};
+              border: ${emotionTheme.colors.border};
+              color: ${emotionTheme.colors.fg};
+            }
+
+            .btn-default { 
+              color: ${emotionTheme.colors.fg};
+              border-color: ${emotionTheme.colors.primarybg};
+              background-color: ${emotionTheme.colors.primarybg};
+            }
+
+            *::-webkit-scrollbar {
+              width: 7px;
+              height: 7px;
+            }
+
+            *::-webkit-scrollbar-thumb {
+              background: ${emotionTheme.colors.scrollbar};
+            }
+
+            *::-webkit-scrollbar-track {
+              background: ${emotionTheme.colors.bg};
+            }
+          </style>
+        \`;
+
+        if(${theme === 'programmers'} && ${webviewUrl.startsWith(`https://${BOJ_DOMAIN}/problem`)}) {
+          document.body.appendChild($newStyleDiv);
+        }
+      })();
+    `,
+      )
+      .catch(console.log);
+  }, [emotionTheme, theme, webview, webviewUrl]);
 
   /**
    * webview 상태 초기화
@@ -30,6 +133,17 @@ export function useWebview() {
     }
 
     newWebview.addEventListener('dom-ready', () => {
+      /**
+       * 백준 허브 확장 프로그램에서 삽입되는 기본 스타일로 인한
+       * 리스트 태그 패딩값이 사라지는 문제를 해결하기 위한 코드
+       */
+      newWebview.insertCSS(`
+        ol,
+        ul:not(.nav),
+        dl {
+          padding-left: 40px;
+        }
+      `);
       setWebview(newWebview);
     });
   }, [setWebview]);
