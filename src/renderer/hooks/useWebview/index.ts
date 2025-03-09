0e7b9b8463800d506613dd73ba9b59a4b97c105f
jeong57281 <jeong5728@gmail.com>
2025-03-09 18:10:27 +0900

    chore: 디버깅 콘솔 출력 삭제

--- src/renderer/hooks/useWebview/index.ts
+++ src/renderer/hooks/useWebview/index.ts
@@ -108,7 +108,6 @@ export function useWebview() {
     }
 
     const handleWebviewDidFinishLoad = async () => {
-      console.log('-------------------------------');
       await refreshWebviewTheme();
 
       updateWebviewLoading('finished');
