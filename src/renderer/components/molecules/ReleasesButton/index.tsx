c7c98b498a0377ac5e815c3ea411f07a8d7439b1
junghyunbak <jeong5728@gmail.com>
2025-03-13 13:07:40 +0900

    chore: 릴리즈버튼 useCallback 적용

--- src/renderer/components/molecules/ReleasesButton/index.tsx
+++ src/renderer/components/molecules/ReleasesButton/index.tsx
@@ -1,11 +1,13 @@
+import { useCallback } from 'react';
+
 import { RELEASE_VERSION } from '@/common/constants';
 
 import { TransparentButton } from '@/renderer/components/atoms/buttons/TransparentButton';
 
 export function ReleasesButton() {
-  const handleReleaseButtonClick = () => {
+  const handleReleaseButtonClick = useCallback(() => {
     window.open('https://github.com/junghyunbak/boj-ide/releases', '_blank');
-  };
+  }, []);
 
   return <TransparentButton onClick={handleReleaseButtonClick} size="small">{`v${RELEASE_VERSION}`}</TransparentButton>;
 }
