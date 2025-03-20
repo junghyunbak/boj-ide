fa64d52a0feaf319ded266523af19bc483088def
junghyunbak <jeong5728@gmail.com>
2025-03-20 21:08:04 +0900

    test: 통합 테스트 환경 준비

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,11 @@
 /**
- * ai/react 패키지 내부 브라우저 종속 TransformStream 사용으로 인해 polyfill 적용
+ * ai/react 패키지 내부 브라우저 종속 TransformStream 사용으로 인한 polyfill
  */
 import 'web-streams-polyfill/polyfill';
+
+/**
+ * DOMRect API 사용 불가로 인한 polyfill
+ */
+import 'geometry-polyfill';
 
 const readable = new ReadableStream();
--- src/__tests__/integration/App.test.tsx
+++ src/__tests__/integration/App.test.tsx
@@ -1,8 +1,35 @@
-import { PaintButton } from '@/renderer/components/atoms/buttons/PaintButton';
-import { render } from '@/renderer/utils';
+import App from '@/renderer/App';
+import { render } from '@testing-library/react';
+
+beforeEach(() => {
+  const tourPortalRoot = document.createElement('div');
+  tourPortalRoot.id = 'tour';
+  document.body.appendChild(tourPortalRoot);
+
+  const afterImagePortalRoot = document.createElement('div');
+  afterImagePortalRoot.id = 'after-image';
+  document.body.appendChild(afterImagePortalRoot);
+
+  window.HTMLElement.prototype.scrollIntoView = jest.fn();
+
+  window.getComputedStyle = jest.fn();
+
+  window.electron = {
+    ipcRenderer: {
+      once(channel, func) {},
+      on(channel, listener) {
+        return () => {};
+      },
+      sendMessage(channel, message) {},
+      invoke(channel, message) {
+        return Promise.resolve(null);
+      },
+    },
+  };
+});
 
 describe('App', () => {
   it('should render', () => {
-    expect(render(<PaintButton />)).toBeTruthy();
+    expect(render(<App />)).toBeTruthy();
   });
 });
