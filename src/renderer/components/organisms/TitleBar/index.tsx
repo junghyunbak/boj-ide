205ca595d5e46051cf05cd78a956ef3ea79d560f
jeong57281 <jeong5728@gmail.com>
2025-03-27 14:24:55 +0900

    chore: 윈도우 환경에서 추가적인 ui 제거

--- src/renderer/components/organisms/TitleBar/index.tsx
+++ src/renderer/components/organisms/TitleBar/index.tsx
@@ -4,8 +4,6 @@ import { size } from '@/common/constants';
 
 import { ProblemHistory } from '@/renderer/components/molecules/ProblemHistory';
 
-import { ReactComponent as Logo } from '@/renderer/assets/svgs/logo.svg';
-
 export function TitleBar() {
   return (
     <div
@@ -23,40 +21,6 @@ export function TitleBar() {
         -webkit-app-region: drag;
       `}
     >
-      {window.electron.platform === 'win32' && (
-        <div
-          css={css`
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-
-            width: 100%;
-          `}
-        >
-          <div
-            css={(theme) => css`
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              color: ${theme.colors.fg};
-              gap: 5px;
-
-              padding-left: 10px;
-            `}
-          >
-            <Logo width="12px" />
-            <p
-              css={css`
-                font-size: 12px;
-                font-family: menlo;
-              `}
-            >
-              BOJ IDE
-            </p>
-          </div>
-        </div>
-      )}
-
       <ProblemHistory />
     </div>
   );
