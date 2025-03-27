ba60a58e93f7e078399d75e42e5dae561c40480e
junghyunbak <jeong5728@gmail.com>
2025-03-27 09:35:59 +0900

    fix: 히스토리 모달, 그림판 크기 유지되지 않는 문제 수정

--- src/renderer/hooks/useLayout/index.ts
+++ src/renderer/hooks/useLayout/index.ts
@@ -1,14 +1,13 @@
-import { useRef } from 'react';
-
 import { useStore } from '@/renderer/store';
 import { useShallow } from 'zustand/shallow';
 
 export function useLayout() {
-  const webviewRatio = useRef(useStore.getState().leftRatio).current;
-  const editorRatio = useRef(useStore.getState().topRatio).current;
-  const paintRatio = useRef(useStore.getState().paintLeftRatio).current;
-
-  const historyModalHeight = useRef(useStore.getState().historyModalHeight).current;
+  const {
+    historyModalHeight,
+    leftRatio: webviewRatio,
+    topRatio: editorRatio,
+    paintLeftRatio: paintRatio,
+  } = useStore.getState();
 
   const [isPaintOpen] = useStore(useShallow((s) => [s.isPaintOpen]));
 
