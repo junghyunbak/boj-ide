6d8452f1dc200b7a4a01bc859cb831a116b27c40
junghyunbak <jeong5728@gmail.com>
2025-04-04 10:12:42 +0900

    feat: 이미지 워터마크 텍스트 추가

--- src/renderer/components/molecules/CaptureCodeButton/index.tsx
+++ src/renderer/components/molecules/CaptureCodeButton/index.tsx
@@ -8,12 +8,16 @@ import { fabric } from 'fabric';
 
 import { ReactComponent as Copy } from '@/renderer/assets/svgs/copy.svg';
 
+const MIN_PADDING = 25;
+const WATERMARK_PADDING = 4;
+
 export function CaptureCodeButton() {
   const { canvas } = usePaint();
   const { fireAlertModal } = useModifyAlertModal();
 
   const emotionTheme = useTheme();
 
+  // TODO: useModifyPaint 커스텀 훅으로 로직 분리
   const handleCreateImageButtonClick = useCallback(async () => {
     if (!canvas) {
       return;
@@ -34,7 +38,14 @@ export function CaptureCodeButton() {
     canvasObjects.forEach((obj) => {
       const { top, left, height, width, scaleX, scaleY } = obj;
 
-      if (!top || !left || !width || !height || !scaleX || !scaleY) {
+      if (
+        top === undefined ||
+        left === undefined ||
+        width === undefined ||
+        height === undefined ||
+        scaleX === undefined ||
+        scaleY === undefined
+      ) {
         return;
       }
 
@@ -47,28 +58,48 @@ export function CaptureCodeButton() {
       mab = Math.max(mab, top + realHeight);
     });
 
-    const paddingX = (mar - mil) * 0.1;
-    const paddingY = (mab - mit) * 0.1;
+    const paddingX = Math.max((mar - mil) * 0.1, MIN_PADDING);
+    const paddingY = Math.max((mab - mit) * 0.1, MIN_PADDING);
 
     const width = mar - mil + paddingX * 2;
     const height = mab - mit + paddingY * 2;
+
+    if (width === Infinity || height === Infinity) {
+      fireAlertModal('오류', '그림을 저장하는 중 문제가 발생했습니다.');
+      return;
+    }
 
     const mainCanvas = new fabric.Canvas('mainCanvas', { backgroundColor: emotionTheme.colors.bg, width, height });
 
     const addObjectToMainCanvas = (obj: fabric.Object) => {
       return new Promise((resolve) => {
         obj.clone((cloneObj: fabric.Object) => {
-          mainCanvas.add(
-            cloneObj.set({
-              left: (cloneObj.left ?? -Infinity) - mil + paddingX,
-              top: (cloneObj.top ?? -Infinity) - mit + paddingY,
-            }),
-          );
+          const { left, top } = cloneObj;
+
+          if (left !== undefined && top !== undefined) {
+            mainCanvas.add(
+              cloneObj.set({
+                left: left - mil + paddingX,
+                top: top - mit + paddingY,
+              }),
+            );
+          }
 
           resolve(true);
         });
       });
     };
+
+    const waterMarkText = new fabric.Text('BOJ IDE', {
+      fontSize: 12,
+      fontFamily: 'hack',
+      fill: emotionTheme.colors.border,
+    });
+
+    waterMarkText.left = mil - paddingX + WATERMARK_PADDING;
+    waterMarkText.top = mit - paddingY + WATERMARK_PADDING;
+
+    await addObjectToMainCanvas(waterMarkText);
 
     for (const obj of canvasObjects) {
       await addObjectToMainCanvas(obj);
